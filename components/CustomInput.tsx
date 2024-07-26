acdae8546c41994a28a7f294bc88fe661147afcb
amr0exe <zyklen908@gmail.com>
2024-07-26 09:34:47 +0545

    implement Auth Page UI

---
+++ components/CustomInput.tsx
@@ -0,0 +1,38 @@
+import { Input } from "./ui/input";
+import { FormField, FormLabel, FormControl, FormMessage } from "./ui/form";
+import { Control, FieldPath, Form } from "react-hook-form";
+import { z } from "zod";
+import { authFormSchema } from "@/lib/utils";
+
+const formSchema = authFormSchema('sign-up')
+
+interface  CustomInput {
+    control: Control<z.infer<typeof formSchema>>,
+    name: FieldPath<z.infer<typeof formSchema>>,
+    label: string,
+    placeholder: string
+}
+
+function CustomInput({ control, name, label, placeholder} : CustomInput ) {
+    return (
+         <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <div className="form-item">
+                    <FormLabel className="form-label">
+                       {label} 
+                    </FormLabel>
+                    <div className="flex w-full flex-col">
+                        <FormControl>
+                            <Input placeholder={placeholder} type={name === 'password' ? 'password' : 'text'} className="input-class" {...field}/>
+                        </FormControl>
+                        <FormMessage className="form-message mt-2" />
+                    </div>
+                </div>
+            )}
+            />
+    )
+}
+
+export default CustomInput
